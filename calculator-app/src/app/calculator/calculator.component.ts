437f1ed15c71a857aa9be3e09e420307f8db689c
Rondel Hallare <hallarerondel@yahoo.com>
2021-03-05 14:16:10 +0800

    minor updates

--- calculator-app/src/app/calculator/calculator.component.ts
+++ calculator-app/src/app/calculator/calculator.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorComponent implements OnInit {
 
   constructor() { }
-  answered = false;
+  answered = false; 
   operatorSet = false;
   operand1: number;
   operand2: number;
@@ -19,90 +19,90 @@ export class CalculatorComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  deleteKey($event) {
-    if ($event.key == 'Delete') {
-      if (this.mainText != "") {
-        this.mainText = this.mainText.substr(0, this.mainText.length - 1);
+  deleteKey($event){
+    if($event.key == 'Delete'){
+      if (this.mainText !="") {
+        this.mainText=this.mainText.substr(0, this.mainText.length-1);
       }
-    }
-  }
-
-  endKey($event) {
-    if ($event.key == 'End') {
-      this.clear();
-    }
-  }
-
-  keyPress(key: string) {
-    if (key === '/' || key === '*' || key === '-' || key === '+') {
-      const lastKey = this.mainText[this.mainText.length - 1];
-      if (lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+') {
-        this.operatorSet = true;
       }
-      if ((this.operatorSet) || (this.mainText === '')) {
+   }
+  
+   endKey($event){
+     if($event.key =='End'){
+       this.clear();
+        }
+     }
+  
+    keyPress(key: string) {
+      if (key === '/' || key === '*' || key === '-' || key === '+') {
+         const lastKey = this.mainText[this.mainText.length - 1];
+         if (lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+')  {
+           this.operatorSet = true;
+         }
+         if ((this.operatorSet) || (this.mainText === '')) {
+           return;
+         }
+         this.operand1 = parseFloat(this.mainText);
+         this.operand2 = parseFloat(this.mainText);
+         this.operator = key;
+         this.operatorSet = true;
+      }
+      if (this.mainText.length === 10) {
         return;
       }
-      this.operand1 = parseFloat(this.mainText);
-      this.operand2 = parseFloat(this.mainText);
-      this.operator = key;
-      this.operatorSet = true;
-    }
-    if (this.mainText.length === 10) {
-      return;
-    }
-    this.mainText += key;
-  }
-
+      this.mainText += key;
+   }
+   
   answer() {
     let formula = this.mainText;
-
+  
     let lastKey = formula[formula.length - 1];
-
-    if (lastKey === '.') {
-      formula = formula.substr(0, formula.length - 1);
+  
+    if (lastKey === '.')  {
+      formula=formula.substr(0,formula.length - 1);
     }
-
+  
     lastKey = formula[formula.length - 1];
-
-    if (lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+' || lastKey === '.') {
-      formula = formula.substr(0, formula.length - 1);
+  
+    if (lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+' || lastKey === '.')  {
+      formula=formula.substr(0,formula.length - 1);
     }
     this.subText = this.mainText;
     this.mainText = eval(formula);
   }
-
-
-  equals() {
-    if (this.mainText != '') {
+  
+  
+   equals() {
+    if(this.mainText != ''){
       this.answer();
-
+  
       var checkLength = `${this.mainText}`;
-
-      if (checkLength.length > 10) {
+  
+      if(checkLength.length > 10){
         document.getElementById('max').style.color = 'white';
       }
-
-      if (this.mainText == "0") this.mainText = "";
-    } else {
+  
+      if (this.mainText=="0") this.mainText="";
+    }else{
       this.clear();
     }
-
+  
   }
-
-
-  clear() {
-    this.mainText = '';
-    this.subText = '';
-    this.operator = '';
-    this.calculationString = '';
-    this.answered = false;
-    this.operatorSet = false;
-    document.getElementById('max').style.color = 'rgba(255, 255, 255, 0.05)';
-  }
-
-  delete() {
-    if (this.mainText != "") {
-      this.mainText = this.mainText.substr(0, this.mainText.length - 1);
+  
+  
+   clear(){
+     this.mainText = '';
+     this.subText = '';
+     this.operator = '';
+     this.calculationString = '';
+     this.answered = false;
+     this.operatorSet = false;
+     document.getElementById('max').style.color = 'rgba(255, 255, 255, 0.05)';
+   }
+  
+   delete(){
+    if (this.mainText !="") {
+      this.mainText=this.mainText.substr(0, this.mainText.length-1);
     }
-  }
+   }
 }
